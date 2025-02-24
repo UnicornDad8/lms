a842853550fd90bca4c5f6d02023b3667031f137
UnicornDad8 <unicorndad8@gmail.com>
2025-02-24 15:33:54 -0300

    Add MobileNavbar component

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import MobileSidebar from './mobile-sidebar';
 
 const Navbar = () => {
   return (
     <div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
-      Navbar
+      <MobileSidebar />
     </div>
   )
 }
